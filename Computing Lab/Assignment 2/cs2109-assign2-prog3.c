13b2db5e2147a4ab767bb688ef538e88aafaaef6
Abhinaba Chakraborty <97806057+cohitherewer@users.noreply.github.com>
2022-01-26 14:24:43 +0530

    Update cs2109-assign2-prog3.c
    Instead of writing a whole C code, wrote some C++ code, with file extention as .c
    Compile it using g++. It should work

--- Computing Lab/Assignment 2/cs2109-assign2-prog3.c
+++ Computing Lab/Assignment 2/cs2109-assign2-prog3.c
@@ -1,87 +1,79 @@
 #include<stdio.h>
 #include<limits.h>
 #include<stdlib.h>
+#include<bits/stdc++.h>
+using namespace std;
 #define N 1000000
 
 int V[N]={0};
+int n;
 struct TreeNode{
 	int data;
 	struct TreeNode *left;struct TreeNode *right;
-	//TreeNode(int x):val(x),left(NULL),right(NULL){}
 };
-struct TreeNode* createBSTutil(int n,int *index,int X,int Y){
+struct TreeNode* createBSTutil(int *index,int X,int Y){
 	if(*index>=n || V[*index]<X || V[*index]>Y)return NULL;
 	struct TreeNode* root=(struct TreeNode *)malloc(sizeof(struct TreeNode *));
 	root->data=V[(*index)++];root->left=NULL,root->right=NULL;
-	root->left=createBSTutil(n,index,X,root->data-1);
-	root->right=createBSTutil(n,index,root->data+1,Y);
+	root->left=createBSTutil(index,X,root->data-1);
+	root->right=createBSTutil(index,root->data+1,Y);
 	return root;
 }
-struct TreeNode* createBST(int n){
+struct TreeNode* createBST(){
 	if(n==0)return NULL;
 	int index=0;
-	return createBSTutil(n,&index,INT_MIN,INT_MAX);
+	return createBSTutil(&index,INT_MIN,INT_MAX);
 }
-//void postOrder(struct TreeNode* root){
-	//if(root==NULL)return;
-	//postOrder(root->left);
-	//postOrder(root->right);
-	//printf("%d ",root->data);
-//}
-int A[2][N]={0};
-void zPattern(struct TreeNode *root,int index,int *x,char dir,int *f){
-	A[index][(*x)++]=root->data;
+void func(struct TreeNode *root,vector<vector<pair<int,int>>>& v,vector<pair<int,int>> T,int dir){
+	if(root==NULL)return;
+	T.push_back(make_pair(root->data,dir));
 	if(root->left==NULL && root->right==NULL){
-		if((*x)>2)*f=1;
-		return;
+		v.push_back(T);
 	}
-	if(dir=='R' && root->right!=NULL)zPattern(root->right,index,x,'L',f);
-	else if(dir=='L' && root->left!=NULL)zPattern(root->left,index,x,'R',f);
-	else return;
+	if(root->left)func(root->left,v,T,0);
+	if(root->right)func(root->right,v,T,1);
+}
+void levelOrder(struct TreeNode *root){
+	vector<vector<pair<int,int>>> v;
+	vector<pair<int,int>> x;x.push_back(make_pair(root->data,-1));
+	if(root->left)func(root->left,v,x,0);
+	if(root->right)func(root->right,v,x,1);
+	vector<vector<pair<int,int>>> ans;int zCount=0;
+	for(int i=0;i<(int)v.size();++i){
+		//for(auto it:v[i])cout<<it.first<<" ";
+		//cout<<"Path: ";int count=0;
+		if((int)v[i].size()<=2)continue;
+		else{
+			for(int j=0;j<(int)v[i].size();){
+				if(v[i][j].second==1 && v[i][j+1].second==0 && v[i][j+2].second==1){++count;j+=3;}
+				else j+=1;
+			}
+			if(count==0)continue;
+			if(count>zCount){
+				ans.clear();ans.push_back(v[i]);zCount=count;
+			}else if(count==zCount && ans.size()>0 && ans[0].size()>v[i].size()){
+				ans.clear();ans.push_back(v[i]);
+			}else if(ans[0].size()==v[i].size()){
+				ans.push_back(v[i]);
+			}	
+		}
+		//cout<<endl;
+	}
+	for(int i=0;i<(int)ans.size();++i){
+		for(auto it:ans[i])cout<<it.first<<" ";
+		cout<<endl;
+	}
 }
 int main()
 {
-	int n;scanf("%d",&n);int a=0;
+	scanf("%d",&n);int a=0;
 	for(;a<n;++a)scanf("%d",&V[a]);
-	struct TreeNode* root=createBST(n);
+	struct TreeNode* root=createBST();
 	//postOrder(root);
 	if(root==NULL){
 		printf("0\n");return 0;
 	}
-	//A[0][0]=root->data;A[1][0]=root->data;
-	int i=0,j=0,f=0,g=0;
-	if(root->left){
-		zPattern(root,0,&i,'L',&f);
-	}
-	if(root->right){
-		zPattern(root,1,&j,'R',&g);
-	}
-	if(f==1 && g==1){
-		if(i>j){
-			int p=0;
-			for(;p<j;++p)p<j-1?printf("%d-",A[1][p]):printf("%d\n",A[1][p]);
-			printf("%d\n",j-1);
-		}else if(j>i){
-			int p=0;
-			for(;p<i;++p)p<i-1?printf("%d-",A[0][p]):printf("%d\n",A[0][p]);
-			printf("%d\n",i-1);
-		}else{
-			int p=0;
-			for(;p<i;++p)p<i-1?printf("%d-",A[0][p]):printf("%d\n",A[0][p]);
-			printf("%d\n",i-1);
-			p=0;
-			for(;p<j;++p)p<j-1?printf("%d-",A[1][p]):printf("%d\n",A[1][p]);
-			printf("%d\n",j-1);
-		}
-	}else if(f==1){
-		int p=0;
-		for(;p<i;++p)p<i-1?printf("%d-",A[0][p]):printf("%d\n",A[0][p]);
-		printf("%d\n",i-1);
-	}else if(g==1){
-		int p=0;
-		for(;p<j;++p)p<j-1?printf("%d-",A[1][p]):printf("%d\n",A[1][p]);
-		printf("%d\n",j-1);
-	}else{
-		printf("0\n");
-	}
+	
+	levelOrder(root);
 }
+
